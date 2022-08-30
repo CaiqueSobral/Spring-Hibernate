a60246071faa406fdd3f5691b9622b0d7f5ee7cf
Caique Sobral <caiquelsobral@gmail.com>
2022-08-30 13:33:46 -0300

    FEAT: Bean scopes - writting some code, part 2

--- spring-demo-one/src/com/luv2code/springdemo/BeanScopeDemoApp.java
+++ spring-demo-one/src/com/luv2code/springdemo/BeanScopeDemoApp.java
@@ -11,5 +11,13 @@ public class BeanScopeDemoApp {
     //retrieve bean from spring container
     Coach theCoach = context.getBean("myCoach", Coach.class);
     Coach alphaCoach = context.getBean("myCoach", Coach.class);
+
+    boolean result = (theCoach == alphaCoach);
+
+    System.out.println("\nPointing to the same object: " + result);
+    System.out.println("\nMemory location for theCoach: " + theCoach);
+    System.out.println("\nMemory location for theCoach: " + alphaCoach + "\n");
+
+    context.close();
   }
 }
